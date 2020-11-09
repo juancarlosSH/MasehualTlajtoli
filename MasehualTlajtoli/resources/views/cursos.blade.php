44be8c2333b647ab7108fd5561d68b5f932ef090
RafaelAndrade <rafithahardy@gmail.com>
2020-11-08 21:34:36 -0600

    Adecuación de estilos para la consulta de cursos

--- MasehualTlajtoli/resources/views/cursos.blade.php
+++ MasehualTlajtoli/resources/views/cursos.blade.php
@@ -1,41 +1,32 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layouts.app')
+@section('content')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Cursos disponibles') }}</div>
 
-    <title>Cursos disponibles</title>
-  </head>
-  <body>
-    <div class="container my-4">
-        <h1 class="display-4">Cursos disponibles</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Descripción</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($cursos_disponibles as $curso)
-                <tr>
-                    <td>{{$curso->name}}</td>
-                    <td>{{$curso->description}}</td>
-                </tr>
-                @endforeach   
-            </tbody>
-        </table>
+                <div class="card-body">
+                <table class="table">
+                  <thead>
+                      <tr>
+                          <th scope="col">Nombre</th>
+                          <th scope="col">Descripción</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      @foreach($cursos_disponibles as $curso)
+                      <tr>
+                          <td>{{$curso->name}}</td>
+                          <td>{{$curso->description}}</td>
+                      </tr>
+                      @endforeach   
+                  </tbody>
+              </table>
+              </div>
+            </div>
+        </div>
     </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-  </body>
-</html>
+</div>
+@endsection
