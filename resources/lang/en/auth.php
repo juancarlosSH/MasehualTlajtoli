08ed8a89a1f8092abfab1585e198b33bc2433a23
RafaelAndrade <rafithahardy@gmail.com>
2020-11-24 19:04:47 -0600

    Modificación de campos de Inicio de sesión y de mensajes de error

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Error, correo electrónico y/o contraseña incorrectos',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,14 +8,14 @@
                         <h1 class="h1 mb-3 font-weight-normal">Inicio de sesión</h1>
                         @csrf
                             <label for="email">{{ __('Correo electrónico') }}</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus maxlength="100">
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             <label for="password">{{ __('Contraseña') }}</label>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" minlength="8" maxlength="32">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
