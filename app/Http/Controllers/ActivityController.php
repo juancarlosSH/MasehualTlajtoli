807b81db7a2d3835ce8e6ef6c3fa37ed49132671
RafaelAndrade <rafithahardy@gmail.com>
2020-12-11 00:02:56 -0600

    Visualización de actividades de los cursos del usuario al 90%

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Course;
+use Illuminate\Pagination\LengthAwarePaginator;
 use App;
 
 class ActivityController extends Controller
@@ -13,10 +15,21 @@ class ActivityController extends Controller
         $this->middleware('auth');
     }
 
-    public function consultar_actividades_curso($id){
-        $id_usuario = Auth::id();
-        $actividades_realizadas = App\Models\ActivityProgress::where('user_id', $id_usuario)->get();
-        $actividades_generales = App\Models\Activity::where('course_id', $id)->get();
-        return view('actividades.actividades_curso', compact('actividades_realizadas', 'actividades_generales'));
+    public function consultar_actividades_curso(Course $course){
+        $activities = $course->activities;
+        $auxiliar = [];
+
+        foreach ($activities as $activity) {
+            //$course = $activity->course;
+            array_push($auxiliar, $activity);
+        }
+
+        $currentPage = LengthAwarePaginator::resolveCurrentPage();
+        $assignedActivities = collect(array_unique($auxiliar));
+        $perPage = 5;
+        $currentPageActivities = $assignedActivities->slice(($currentPage * $perPage) - $perPage, $perPage)->all();
+        $paginatedActivities= new LengthAwarePaginator($currentPageActivities , count($assignedActivities), $perPage);
+        $paginatedActivities->setPath(route('actividades.actividades_curso', $course));
+        return view('actividades.actividades_curso', compact('paginatedActivities'));
     }
 }
